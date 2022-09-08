cd8a386c8a8846d66ce2e08f6b55b36a129d5024
bather2 <2LACCOleib@gmail.com>
2022-09-08 20:52:23 +0900

    ログイン機能作成

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -22,6 +23,16 @@ class AuthController extends Controller
      */
     public function login(LoginFormRequest $request)
     {
-        dd($request->all());
+        $input = $request->only('email', 'password');
+
+        if (Auth::attempt($input)) {
+            $request->session()->regenerate();
+
+            return redirect('home')->with('login_success', 'ログインが成功しました！');
+        }
+
+        return back()->withErrors([
+            'login_error' => 'メールアドレスかパスワードが間違っています。',
+        ]);
     }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ホーム画面</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+  <div class="container">
+    <div class="mt-5">
+      @if (session('login_success'))
+        <div class="bg-green-400 text-center py-4 lg:px-4">
+          <div class="p-2 bg-green-600 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+            <span class="font-semibold mr-2 text-left flex-auto">
+              {{ session('login_success') }}
+            </span>
+          </div>
+        </div>
+      @endif
+      <h3>プロフィール</h3>
+      <ul>
+        <li>名前: {{ Auth::user()->name }}</li>
+        <li>メールアドレス: {{ Auth::user()->email }}</li>
+      </ul>
+    </div>
+  </div>
+</body>
+</html>
